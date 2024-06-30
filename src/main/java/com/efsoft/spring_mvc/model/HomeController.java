b45c08e347a2a49d13759b204f55d7846a2a48f6
Nib1sin <38408560+Nib1sin@users.noreply.github.com>
2024-06-30 16:32:04 +0200

    ENH - Configuracion del MVC en Spring

--- src/main/java/com/efsoft/spring_mvc/model/HomeController.java
+++ src/main/java/com/efsoft/spring_mvc/model/HomeController.java
@@ -1,5 +1,28 @@
 package com.efsoft.spring_mvc.model;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class HomeController {
+	
+	@ModelAttribute
+	public void modelData(Model m){
+		m.addAttribute("name", "Aliens");
+	}
+	
+	@RequestMapping("/")
+	public String home(){
+		return "index";
+	}
+	
+	@RequestMapping("addAlien")
+	public String addAlien(@ModelAttribute("alien") Alien a) {
+		return "result";
+	}
+	
 
 }
